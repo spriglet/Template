1926a5223240484f7abef354250d818841be0f0b
spriglet <redfroglet@gmail.com>
2019-03-23 22:23:33 +0900

    Edited server.js

--- models/connection.js
+++ models/connection.js
@@ -12,7 +12,7 @@ let password = 'Staticpen774@';  //
 let host = ' us-cdbr-iron-east-03.cleardb.net';
 let database = 'heroku_7ee3abcadd72651';
 let username = 'b965c9f20b5784';
-let password = '90ac0673';
+let password = '0f2c0e9a';
 
 // Option 1: Passing parameters separately
 const sequelize = new Sequelize(database, username, password, {
