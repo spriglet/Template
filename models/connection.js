09649e2cdf92e9da40ff01469f59103ef14e1588
spriglet <redfroglet@gmail.com>
2019-03-23 23:03:00 +0900

    Edited connection.js

--- models/connection.js
+++ models/connection.js
@@ -14,11 +14,21 @@ let database = 'heroku_7ee3abcadd72651';
 let username = 'b965c9f20b5784';
 let password = '0f2c0e9a';
 
-// Option 1: Passing parameters separately
-const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, username, password, {
 
-    dialect:'mysql'
-});
+
+if (process.env.CLEARDB_DATABASE_UR) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_UR, {
+        dialect:  'mysql',
+        protocol: 'mysql',
+        logging:  true //false
+    });
+} else {
+    // the application is executed on the local machine
+    sequelize = new Sequelize("postgres:///my_db");
+}
+
+
 sequelize
     .authenticate()
     .then(() => {
